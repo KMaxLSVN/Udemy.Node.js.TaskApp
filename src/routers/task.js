3aa0ba6ece8bcca93e94a24d0765882df6e0cb90
Max Kim <maxim250291@gmail.com>
2023-07-26 21:11:24 +0300

    section 13 - sorting, pagination and filtering

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,9 +17,32 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=10
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === 'disc' ? -1 : 1;
+  }
+
   try {
-    await req.user.populate('tasks');
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
+    });
     res.send(req.user.tasks);
   } catch (error) {
     res.status(500).send();
@@ -84,7 +107,7 @@ router.delete('/tasks/:id', auth, async (req, res) => {
     }
 
     res.send(task);
-  } catch (error) { 
+  } catch (error) {
     res.status(500).send(error);
   }
 });
